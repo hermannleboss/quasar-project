07502e169a1b4885b1c2c014f8f8ba7fb93c8524
Hermann EHO <hermannleboss@gmail.com>
2022-11-23 09:06:16 +0000

    feat: create User Posts and Profile page
    Create pages with quasar cli and linked it with the router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,36 @@
 
+import User from 'layouts/UserLayout.vue'
+import Profile from 'pages/ProfilePage.vue'
+import Posts from 'pages/PostsPage.vue'
+
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
+    ]
+  },
+  {
+    path: '/user',
+
+    // we use /src/layouts/User component which is imported above
+    component: User,
+
+    // hey, it has children routes and User has <router-view> in it;
+    // It is really a Layout then!
+    children: [
+      // Profile page
+      {
+        path: 'profile', // here it is, route /user/profile
+        component: Profile // we reference /src/pages/Profile.vue imported above
+      },
+
+      // Posts page
+      {
+        path: 'posts', // here it is, route /user/posts
+        component: Posts // we reference /src/pages/Posts.vue imported above
+      }
     ]
   },
 
